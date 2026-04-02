cd5dc36ae8e66c74b4e3e77380c5039a7105874c
SÉLala Qasimova <lalaqasim936@gmail.com>
2026-04-02 18:05:12 +0000

    Fix Task 0: safe_print_list

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,14 +1,12 @@
 def safe_print_list(my_list=[], x=0):
-    i = 0
-    printed = 0
+    count = 0
 
-    while i < x:
+    for i in range(x):
         try:
             print(my_list[i], end="")
-            printed += 1
-        except:
+            count += 1
+        except IndexError:
             break
-        i += 1
 
     print()
-    return printed
+    return count
