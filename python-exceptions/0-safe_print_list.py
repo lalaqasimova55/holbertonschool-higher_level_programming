2412346a77d81f3f1a929e3ca7d7b32feea86a1c
SÉLala Qasimova <lalaqasim936@gmail.com>
2026-04-02 17:50:34 +0000

    Task 0: safe_print_list

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+def safe_print_list(my_list=[], x=0):
+    i = 0
+    printed = 0
+
+    while i < x:
+        try:
+            print(my_list[i], end="")
+            printed += 1
+        except:
+            break
+        i += 1
+
+    print()
+    return printed
