259c9a92a244df9d74b2d26b539104d5add76aff
Lala Qasimova <lalaqasim936@gmail.com>
2026-05-14 22:25:50 +0000

    Advanced task: 100-move_to_utf8

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -7,7 +7,3 @@ COLLATE utf8mb4_unicode_ci;
 ALTER TABLE hbtn_0c_0.first_table
 CONVERT TO CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
-
-ALTER TABLE hbtn_0c_0.first_table
-MODIFY name VARCHAR(256)
-COLLATE utf8mb4_unicode_ci;
