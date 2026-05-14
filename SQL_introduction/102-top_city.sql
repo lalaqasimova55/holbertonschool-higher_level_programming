4d6b1c1353675eabe570a7144658888bc08ada63
Lala Qasimova <lalaqasim936@gmail.com>
2026-05-14 22:29:33 +0000

    Advanced task : 102-top_city

---
+++ SQL_introduction/102-top_city.sql
@@ -0,0 +1,7 @@
+-- displays top 3 cities by average temperature in July and August
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures
+WHERE month IN (7, 8)
+GROUP BY city
+ORDER BY avg_temp DESC
+LIMIT 3;
